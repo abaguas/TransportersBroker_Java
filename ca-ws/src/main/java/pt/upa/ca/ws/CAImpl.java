13edb51d8fc4a69da90db805e399b146441fda66
abaguas <andre.b.aguas@gmail.com>
2016-05-09 17:48:59 +0100

    certificados publicos no CA

--- ca-ws/src/main/java/pt/upa/ca/ws/CAImpl.java
+++ ca-ws/src/main/java/pt/upa/ca/ws/CAImpl.java
@@ -5,6 +5,10 @@ import javax.jws.WebService;
 @WebService(endpointInterface = "pt.upa.ca.ws.CA")
 public class CAImpl implements CA {
 
+	
+	
+	
+	
 	public String sayHello(String name) {
 		return "Hello " + name + "!";
 	}
